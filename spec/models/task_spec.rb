87ad65b3d39f66dab1c8c16dc66220665546c275
takumifunayama <mt.koala01n@gmail.com>
2020-05-20 18:16:42 +0900

    [Add]バリデーションテスト実装

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
-RSpec.describe Task, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe 'バリデーション確認', type: :model do
+  describe 'validation' do
+    let(:task){ FactoryBot.create(:task, title: 'title1', status: 'todo')}
+
+    it 'バリデーションが通っていること' do
+      expect(task).to  be_valid
+    end
+
+    it 'タイトルが入力されていること' do
+      task.title = nil
+      expect(task).to_not be_valid
+      expect(task.errors.messages[:title]).to be_present
+    end
+
+    it 'ステータスが入力されていること' do
+      task.status = nil
+      expect(task).to_not be_valid
+      expect(task.errors.messages[:status]).to be_present
+    end
+  end
 end
